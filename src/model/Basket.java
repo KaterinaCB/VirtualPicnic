8b09361ebf58d3b407cbb382e512c9fcca2742c6
ceb <vivu1991@mail.ru>
2024-04-17 17:22:30 +0300

    Промежуточная контрольная работа

---
+++ src/model/Basket.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Map;
+
+public interface Basket<T> extends Iterable<T>{
+    int getBasketSize();
+    void addToBasket(T plant);
+    Integer getPlantAmount(T plant);
+
+    Map<T, Integer> getBasket();
+}
